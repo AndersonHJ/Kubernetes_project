6a12beee0366c3cef072091493c234c733c487f6
AndersonHJ <houjian.max@gmail.com>
2016-09-30 02:10:53 +0000

    news

--- ang.js
+++ ang.js
@@ -1,1 +1,29 @@
 
+angular.module("meapp", []).controller('mecontroller', function($scope, $http) {
+            $scope.submit = function(){
+               
+               var data = {
+                  fname: $scope.firstName,
+                  lname: $scope.lastName,
+                  bir: $scope.birthday,
+                  houseNumber : $scope.houseNum,
+                  street: $scope.street,
+                  city: $scope.city
+               };
+               $scope.res = data;
+
+               // $http.post("104.199.119.167", data).then(function(response){
+               //    $scope.res = response;
+               // });
+            }
+
+            $scope.cancel = function(){
+            	$scope.firstName = "";
+            	$scope.lastName = "";
+            	$scope.birthday = "";
+            	$scope.houseNum = "";
+            	$scope.street = "";
+            	$scope.city = "";
+            }
+            
+         });
