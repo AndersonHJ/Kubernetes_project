6e2dc1a822cf2c3284e2286b839a903e4a216bee
AndersonHJ <houjian.max@gmail.com>
2016-10-14 00:20:16 +0000

    HW5_1.1

--- m.webTool_angularJS/controller.js
+++ m.webTool_angularJS/controller.js
@@ -1,29 +1,116 @@
 
 var angMod = angular.module("meapp", []);
+
 angMod.controller('mecontroller', function($scope, $http) {
-            $scope.res = [];
-            $scope.submit = function(){
-               var arr = [];
-               var data = {
-                  fname: $scope.firstName,
-                  lname: $scope.lastName,
-                  bir: $scope.birthday,
-                  houseNumber : $scope.houseNum,
-                  street: $scope.street,
-                  city: $scope.city
-               };
-               
-               $scope.res.push(data);
+    $scope.res = [];
+    $scope.submit = function(){
+       $scope.result = {
+            firstName : "",
+            lastName : "",
+            birthday: "",
+            houseNum: "",
+            street: "",
+            city: ""
+       };
 
-            }
-
-            $scope.cancel = function(){
-            	$scope.firstName = "";
-            	$scope.lastName = "";
-            	$scope.birthday = "";
-            	$scope.houseNum = "";
-            	$scope.street = "";
-            	$scope.city = "";
-            }
+        angular.forEach($scope.info, function(value, key){
+            //console.log(key+" : "+value);
+            $http.get("map.php?cmd=set&key="+key+"&value="+value)
+            .success(function (data, status, headers, config) {
+                $scope.redisResponse = "Updated.";
+            });
+            $http.get("map.php?cmd=get&key="+key)
+                .success(function (data, status, headers, config) {
+                    $scope.result[key] = data.data;
+                    //console.log(data.data);
+                })
+                .error(function (data, status, header, config) {
+                    console.log(data);
+            });
             
-         });
+        });
+        
+        $scope.res.push($scope.result);
+
+    }
+
+    $scope.cancel = function(){
+      $scope.firstName = "";
+      $scope.lastName = "";
+      $scope.birthday = "";
+      $scope.houseNum = "";
+      $scope.street = "";
+      $scope.city = "";
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $http({
+        //     method: 'JSONP',
+        //     url: 'http://40.124.12.183/map.php?cmd=get&key=name',
+        //     params: {
+        //       format: 'jsonp',
+        //       json_callback: 'JSON_CALLBACK'
+        //     }
+        // }).success(function (data, status, headers, config) {
+        //         $scope.houseNum = status;
+        //         info.fname = data;
+        //         console.log("success");
+        //         console.log(data);
+        //     })
+        //     .error(function (data, status, header, config) {
+        //         $scope.houseNum = status;
+        //         console.log("asdf"+data);
+        //         info.fname = data;
+        //         console.log("error"+status);
+        //     });
+
+
+
+// angMod.config(function($httpProvider){
+
+//             $httpProvider.defaults.useXDomain = true;
+//             $httpProvider.defaults.withCredentials = true;
+//             delete $httpProvider.defaults.headers.common["X-Requested-With"];
+//             //$httpProvider.defaults.headers.common["Accept"] = "application/json";
+//             $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+//             delete $httpProvider.defaults.headers.common["Origin"];
+//             delete $httpProvider.defaults.headers.common["Referer"];
+//             $httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+
+//             console.log($httpProvider.defaults.headers);
+//                     });
--- webTool_angularJS/controller.js
+++ webTool_angularJS/controller.js
@@ -1,64 +1,85 @@
 
 var angMod = angular.module("meapp", []);
-// angMod.config(function($httpProvider){
 
-//             $httpProvider.defaults.useXDomain = true;
-//             $httpProvider.defaults.withCredentials = true;
-//             delete $httpProvider.defaults.headers.common["X-Requested-With"];
-//             //$httpProvider.defaults.headers.common["Accept"] = "application/json";
-//             $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
-//             delete $httpProvider.defaults.headers.common["Origin"];
-//             delete $httpProvider.defaults.headers.common["Referer"];
-//             $httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-
-//             console.log($httpProvider.defaults.headers);
-//                     });
 angMod.controller('mecontroller', function($scope, $http) {
     $scope.res = [];
     $scope.submit = function(){
-       var info = {
-          // fname: $scope.firstName,
-          // lname: $scope.lastName,
-          // bir: $scope.birthday,
-          // houseNumber : $scope.houseNum,
-          // street: $scope.street,
-          // city: $scope.city
+       $scope.result = {
+            firstName : "",
+            lastName : "",
+            birthday: "",
+            houseNum: "",
+            street: "",
+            city: ""
        };
-       
 
-       // $http.get("http://40.124.12.183/map.php?cmd=set&key=name&value="+$scope.firstName)
-       //      .success(function (data, status, headers, config) {
-       //          $scope.redisResponse = "Updated.";
-       //      });
-
-       
-        $http({
-            method: 'GET',
-            url: 'map.php?cmd=get&key=prof',
-            headers: {
-
-                // 'Access-Control-Allow-Origin' : 'http://40.124.12.183/',
-                // 'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
-                //'Content-Type': 'application/json',
-                //'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-                //'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-            }
-            // headers:{
-            //         
-        }).success(function (data, status, headers, config) {
-                $scope.houseNum = status;
-                info.fname = data;
-                console.log("success");
-                console.log(data);
-            })
-            .error(function (data, status, header, config) {
-                $scope.houseNum = status;
-                console.log("asdf"+data);
-                info.fname = data;
-                console.log(header);
+        angular.forEach($scope.info, function(value, key){
+            //console.log(key+" : "+value);
+            $http.get("map.php?cmd=set&key="+key+"&value="+value)
+            .success(function (data, status, headers, config) {
+                $scope.redisResponse = "Updated.";
             });
+            $http.get("map.php?cmd=get&key="+key)
+                .success(function (data, status, headers, config) {
+                    $scope.result[key] = data.data;
+                    //console.log(data.data);
+                })
+                .error(function (data, status, header, config) {
+                    console.log(data);
+            });
+            
+        });
+        
+        $scope.res.push($scope.result);
 
-        // $http({
+    }
+
+    $scope.cancel = function(){
+    	$scope.firstName = "";
+    	$scope.lastName = "";
+    	$scope.birthday = "";
+    	$scope.houseNum = "";
+    	$scope.street = "";
+    	$scope.city = "";
+    }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $http({
         //     method: 'JSONP',
         //     url: 'http://40.124.12.183/map.php?cmd=get&key=name',
         //     params: {
@@ -78,43 +99,18 @@ angMod.controller('mecontroller', function($scope, $http) {
         //         console.log("error"+status);
         //     });
 
-        $scope.res.push(info);
 
-    }
 
-    $scope.cancel = function(){
-    	$scope.firstName = "";
-    	$scope.lastName = "";
-    	$scope.birthday = "";
-    	$scope.houseNum = "";
-    	$scope.street = "";
-    	$scope.city = "";
-    }
+// angMod.config(function($httpProvider){
 
-    });
+//             $httpProvider.defaults.useXDomain = true;
+//             $httpProvider.defaults.withCredentials = true;
+//             delete $httpProvider.defaults.headers.common["X-Requested-With"];
+//             //$httpProvider.defaults.headers.common["Accept"] = "application/json";
+//             $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+//             delete $httpProvider.defaults.headers.common["Origin"];
+//             delete $httpProvider.defaults.headers.common["Referer"];
+//             $httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
-// var redisApp = angular.module('redis', ['ui.bootstrap']);
-
-// function RedisController() {}
-
-// RedisController.prototype.onRedis = function() {
-//     this.scope_.messages.push(this.scope_.msg);
-//     var value = this.scope_.msg;
-//     this.http_.get("app.php?cmd=append&key=messages&value=" + value)
-//             .success(angular.bind(this, function(data) {
-//                 this.scope_.redisResponse = "Updated.";
-//             }));
-// };
-
-// redisApp.controller('RedisCtrl', function ($scope, $http, $location) {
-//         $scope.controller = new RedisController();
-//         $scope.controller.scope_ = $scope;
-//         $scope.controller.location_ = $location;
-//         $scope.controller.http_ = $http;
-
-//         $scope.controller.http_.get("app.php?cmd=get&key=messages")
-//             .success(function(data) {
-//                 console.log(data);
-//                 $scope.messages = data.data.split(",");
-//             });
-// });
+//             console.log($httpProvider.defaults.headers);
+//                     });
